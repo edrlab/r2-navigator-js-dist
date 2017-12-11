5dcd9baca7c902dcfded81a20a84721c028ce327
danielweck <daniel.weck@gmail.com>
2017-12-11 17:44:43 +0000

    add new dist

---
+++ dist/es7-es2016/src/electron/main/lsd.d.ts
@@ -0,0 +1,5 @@
+import { Server } from "r2-streamer-js/dist/es7-es2016/src/http/server";
+import { Publication } from "r2-streamer-js/dist/es7-es2016/src/models/publication";
+import { IDeviceIDManager } from "./lsd-deviceid-manager";
+export declare function installLsdHandler(publicationsServer: Server, deviceIDManager: IDeviceIDManager): void;
+export declare function launchStatusDocumentProcessing(publication: Publication, publicationPath: string, deviceIDManager: IDeviceIDManager, onStatusDocumentProcessingComplete: () => void): Promise<void>;
