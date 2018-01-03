f204a5ca44345a0fa6c9a806ce682f763aa1b169
danielweck <daniel.weck@gmail.com>
2018-01-03 14:54:03 +0000

    add new dist

---
+++ dist/es6-es2015/src/electron/main/lsd-injectlcpl.d.ts
@@ -0,0 +1,3 @@
+import { Publication } from "r2-shared-js/dist/es6-es2015/src/models/publication";
+export declare function lsdLcpUpdateInject(lcplStr: string, publication: Publication, publicationPath: string): Promise<string>;
+export declare function lsdLcpUpdate(lsdJson: any, publication: Publication): Promise<string>;
