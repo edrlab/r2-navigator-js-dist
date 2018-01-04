18aeab2190f5ba99349626f6e045d039d35c2cec
danielweck <daniel.weck@gmail.com>
2018-01-04 08:17:33 +0000

    add new dist

--- dist/es6-es2015/src/electron/renderer/index.d.ts
+++ dist/es6-es2015/src/electron/renderer/index.d.ts
@@ -1,1 +1,6 @@
+import { Publication } from "r2-shared-js/dist/es6-es2015/src/models/publication";
+export declare function setReadiumCssJsonGetter(func: () => string): void;
+export declare function readiumCssOnOff(): void;
 export declare function handleLink(href: string, previous: boolean | undefined, useGoto: boolean): void;
+export declare function installNavigatorDOM(publication: Publication, publicationJsonUrl: string, rootHtmlElementID: string, preloadScriptPath: string, pubDocHrefToLoad: string, pubDocSelectorToGoto: string): void;
+export declare function navLeftOrRight(left: boolean): void;
--- dist/es7-es2016/src/electron/renderer/index.d.ts
+++ dist/es7-es2016/src/electron/renderer/index.d.ts
@@ -1,1 +1,6 @@
+import { Publication } from "r2-shared-js/dist/es7-es2016/src/models/publication";
+export declare function setReadiumCssJsonGetter(func: () => string): void;
+export declare function readiumCssOnOff(): void;
 export declare function handleLink(href: string, previous: boolean | undefined, useGoto: boolean): void;
+export declare function installNavigatorDOM(publication: Publication, publicationJsonUrl: string, rootHtmlElementID: string, preloadScriptPath: string, pubDocHrefToLoad: string, pubDocSelectorToGoto: string): void;
+export declare function navLeftOrRight(left: boolean): void;
