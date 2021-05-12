f2bbf5b1d292f35a8b7ea2c820765a883305c2a1
Daniel Weck <daniel.weck@gmail.com>
2021-05-12 15:06:39 +0100

    add new dist

--- dist/es5/src/electron/main/lcp.d.ts
+++ dist/es5/src/electron/main/lcp.d.ts
@@ -1,2 +1,2 @@
 import { Server } from "r2-streamer-js/dist/es5/src/http/server";
-export declare function doTryLcpPass(publicationsServer: Server, publicationFilePath: string, lcpPasses: string[], isSha256Hex: boolean): Promise<unknown>;
+export declare function doTryLcpPass(publicationsServer: Server, publicationFilePath: string, lcpPasses: string[], isSha256Hex: boolean): Promise<void>;
